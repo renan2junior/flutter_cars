2a605ce0679f91e9feaa89911c3e98b5dbb8f34f
Renan Junior <renanjunior@renanjunior-RF511-RF411-RF711>
2020-05-08 11:38:24 -0300

    116. Salvando o índice da Tab com shared_preferences
    - Usando Future para retornar o indice.

--- flutter_cars/lib/pages/home_page.dart
+++ flutter_cars/lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cars/pages/carros_listview.dart';
 import 'package:flutter_cars/services/carro_api.dart';
 import 'package:flutter_cars/widgets/drawer_list.dart';
+import 'package:flutter_cars/utils/prefs.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -10,33 +11,53 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    Future<int> future = Prefs.getInt("tab");
+
+    future.then((int tabIndex) {
+      _tabController.index = tabIndex;
+      print(tabIndex);
+    });
+
+    _tabController.addListener(() {
+      print("tab ${_tabController.index}");
+      Prefs.setInt("tab", _tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-          length: 3,
-          child: Scaffold(
-        appBar: AppBar(
-          title: Text("Carros"),
-          bottom: TabBar(tabs: [
-            Tab(
-              text: "Clássicos",
-            ),
-            Tab(
-              text: "Esportivos",
-            ),
-            Tab(
-              text: "Luxo",
-            ),
-          ]),
-        ),
-        body: TabBarView(children: <Widget>[
-          CarrosListView(TipoCarro.classicos),          
-          CarrosListView(TipoCarro.esportivos),          
-          CarrosListView(TipoCarro.luxo),          
-        ],),
-        drawer: DrawerList(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        bottom: TabBar(controller: _tabController, tabs: [
+          Tab(
+            text: "Clássicos",
+          ),
+          Tab(
+            text: "Esportivos",
+          ),
+          Tab(
+            text: "Luxo",
+          ),
+        ]),
       ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CarrosListView(TipoCarro.classicos),
+          CarrosListView(TipoCarro.esportivos),
+          CarrosListView(TipoCarro.luxo),
+        ],
+      ),
+      drawer: DrawerList(),
     );
   }
 }
