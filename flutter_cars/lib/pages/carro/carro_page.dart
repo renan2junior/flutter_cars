14f12a97204c73a2515e5fb48988ae05e753b7d4
Renan Junior <renanjunior@renanjunior-RF511-RF411-RF711>
2020-05-11 12:29:46 -0300

    141. Carro Detalhes - layout parte 2 - scroll

--- flutter_cars/lib/pages/carro/carro_page.dart
+++ flutter_cars/lib/pages/carro/carro_page.dart
@@ -43,57 +43,79 @@ class CarroPage extends StatelessWidget {
           )
         ],
       ),
-      body: _body(carro),
+      body: _body(),
     );
   }
 
-  _body(Carro carro) {
+  _body() {
     return Container(
       padding: EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
           Image.network(carro.urlFoto),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    carro.nome,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    carro.tipo,
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      size: 30,
-                      color: Colors.red,
-                    ),
-                    onPressed: _onClickFavotite,
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.share),
-                    onPressed: _onClickShare,
-                  )
-                ],
-              ),
-            ],
-          )
+          _bloco1(),
+          SizedBox(
+            height: 10,
+          ),
+          _bloco2(),
         ],
       ),
+    );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              carro.nome,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              carro.tipo,
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                size: 30,
+                color: Colors.red,
+              ),
+              onPressed: _onClickFavotite,
+            ),
+            IconButton(
+              icon: Icon(Icons.share),
+              onPressed: _onClickShare,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  Column _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(carro.descricao),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+            "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."),
+      ],
     );
   }
 
