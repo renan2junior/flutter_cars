10910d2af081ac3d019fafeb155089907ee6856a
Renan Junior <renanjunior@renanjunior-RF511-RF411-RF711>
2020-05-15 13:01:28 -0300

    157. Cache com banco de dados

--- flutter_cars/lib/pages/carro/carros_bloc.dart
+++ flutter_cars/lib/pages/carro/carros_bloc.dart
@@ -1,10 +1,18 @@
 import 'package:flutter_cars/models/carro.dart';
+import 'package:flutter_cars/pages/favoritos/carro_dao.dart';
 import 'package:flutter_cars/services/carro_api.dart';
+import 'package:flutter_cars/utils/network.dart';
 import 'package:flutter_cars/utils/simple_bloc.dart';
 
 class CarrosBloc extends SimpleBloc<List<Carro>> {
   Future<List<Carro>> loadCarros(tipo) async {
     try {
+      bool netWorkOn = await isNetworkOn();
+      if(!netWorkOn ){
+        List<Carro> carros = await CarroDAO().findAllByTipo(tipo);
+        add(carros);
+        return carros;
+      }
       List<Carro> carros = await CarroApi.getCarros(tipo);
       add(carros);
       return carros;
