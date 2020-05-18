f05746af546c3a5ecc6746770f336c42730d6578
Renan Junior <renanjunior@renanjunior-RF511-RF411-RF711>
2020-05-18 11:34:07 -0300

    Update carros_bloc.dart

--- flutter_cars/lib/pages/carro/carros_bloc.dart
+++ flutter_cars/lib/pages/carro/carros_bloc.dart
@@ -8,14 +8,17 @@ class CarrosBloc extends SimpleBloc<List<Carro>> {
   Future<List<Carro>> loadCarros(tipo) async {
     try {
       bool netWorkOn = await isNetworkOn();
-      if(!netWorkOn ){
+      if (!netWorkOn) {
         List<Carro> carros = await CarroDAO().findAllByTipo(tipo);
         add(carros);
         return carros;
       }
       List<Carro> carros = await CarroApi.getCarros(tipo);
+
       final dao = CarroDAO();
-      carros.forEach(dao.save);     
+      if (carros.isNotEmpty) {
+        carros.forEach(dao.save);
+      }
       add(carros);
       return carros;
     } catch (error) {
