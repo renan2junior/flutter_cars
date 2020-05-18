9c8665d521be1253b2cddc328b9e517aaf25a0c8
Renan Junior <renanjunior@renanjunior-RF511-RF411-RF711>
2020-05-18 19:09:41 -0300

    174. Http Helper e Token JWT

--- flutter_cars/lib/services/carro_api.dart
+++ flutter_cars/lib/services/carro_api.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter_cars/models/carro.dart';
-import 'package:flutter_cars/models/usuario.dart';
 import 'package:flutter_cars/services/api_response.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_cars/utils/http_helper.dart' as http;
 
 class TipoCarro {
   static final String classicos = "classicos";
@@ -14,39 +13,24 @@ class TipoCarro {
 class CarroApi {
 
   static Future<ApiResponse<bool>> delete(Carro carro) async {
-    Usuario user = await Usuario.get();
-
-    Map<String, String> headers = {
-      "Authorization":" Bearer ${user.token}"
-    };
-
     var url = "http://carros-springboot.herokuapp.com/api/v2/carros/${carro.id}";
-
-    var response = await http.delete(url, headers: headers);
-
+    var response = await http.delete(url);
     if(response.statusCode == 200 ){
       return ApiResponse.ok(true);
     }
     Map mapResponse  = json.decode(response.body);
     return ApiResponse.error(mapResponse["error"]);
-
   }
 
   static Future<ApiResponse<bool>> save(Carro carro) async {
-    Usuario user = await Usuario.get();
-
-    Map<String, String> headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer ${user.token}"
-    };
     String stringJson = carro.toJson();
     var url = "http://carros-springboot.herokuapp.com/api/v2/carros";
     if (carro.id != null) {
       url = url + "/${carro.id}";
     }
     var response = await (carro.id == null
-        ? http.post(url, body: stringJson, headers: headers)
-        : http.put(url, body: stringJson, headers: headers));
+        ? http.post(url, body: stringJson)
+        : http.put(url, body: stringJson));
     if (response.statusCode == 201 || response.statusCode == 200) {
       Map mapResponse = json.decode(response.body);
       Carro carro = Carro.fromMap(mapResponse);
@@ -58,15 +42,10 @@ class CarroApi {
   }
 
   static Future<List<Carro>> getCarros(String tipo) async {
-    Usuario user = await Usuario.get();
     String url =
         "http://carros-springboot.herokuapp.com/api/v2/carros/tipo/$tipo";
-    Map<String, String> _header = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer ${user.token}"
-    };
     print(url);
-    var response = await http.get(url, headers: _header);
+    var response = await http.get(url);
     String _json = response.body;
     try {
       List list = json.decode(_json);
